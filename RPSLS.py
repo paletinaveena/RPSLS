32b94b952bc5e543d5617be2304035aecd57c911
Paleti Naveena <76488426+paletinaveena@users.noreply.github.com>
2023-06-12 11:37:02 +0530

    RPSLS.py

--- RPSLS.py
+++ RPSLS.py
@@ -1,79 +1,56 @@
-# Rock-paper-Scissors-lizard-Spock_game
 import random
-# 0 - rock
-# 1 - Spock
-# 2 - paper
-# 3 - lizard
-# 4 - scissors    
-def name_to_number(name):
-    if name == "rock":
-        return 0
-    elif name == "Spock":
-        return 1
-    elif name == "paper":
-        return 2
-    elif name == "lizard":
-        return 3
-    else:
-        return 4
-    
-def number_to_name(number):
-    if number == 0:
-        return "rock"
-    elif number == 1:
-        return "Spock"
-    elif number == 2:
-        return "paper"
-    elif number == 3:
-        return "lizard"        
-    else:
-        return "scissors"
 
+choices = ["rock", "spock", "paper", "lizard", "scissors"]
 
-def rpsls(player_choice):  
-    print("=>Player choose", player_choice)
-    player_number = name_to_number(player_choice)
-    comp_number = random.randrange(5)
-    comp_choice = number_to_name(comp_number)
-    print("=>Computer choose", comp_choice)
-    difference = (comp_number - player_number) % 5
-    return difference      
-    
+def determine_winner(player_choice, computer_choice):
+    difference = (choices.index(computer_choice) - choices.index(player_choice.lower())) % 5
+    if difference == 0:
+        return "tie"
+    elif difference in [1, 2]:
+        return "computer"
+    else:
+        return "player"
+
+def play_game(num_turns):
+    player_score = 0
+    computer_score = 0
+
+    for _ in range(num_turns):
+        player_choice = input("Enter your choice (rock, spock, paper, lizard, scissors): ")
+        computer_choice = random.choice(choices)
+
+        print("=> Player chooses", player_choice)
+        print("=> Computer chooses", computer_choice)
+
+        winner = determine_winner(player_choice, computer_choice)
+
+        if winner == "tie":
+            player_score += 1
+            computer_score += 1
+            print("*** It's a tie! ***")
+        elif winner == "computer":
+            computer_score += 1
+            print("*** Computer wins! ***")
+        else:
+            player_score += 1
+            print("*** Player wins! ***")
+
+        print()
+
+    print("********* Score Board *********")
+    print("Number of Games Played:", num_turns)
+    print("Player Score:", player_score)
+    print("Computer Score:", computer_score)
+
+    if computer_score > player_score:
+        print("Computer wins the game over", num_turns, "turns")
+    elif player_score > computer_score:
+        print("Player wins the game over", num_turns, "turns")
+    else:
+        print("The game is a tie over", num_turns, "turns")
+
 # Game Starts Here!!
-player = 0
-computer = 0
-n = int(input("Enter number of turns:"))
+num_turns = int(input("Enter number of turns: "))
 print()
-for i in range(n):
-    test = input("Enter object:")
-    diff = rpsls(test)
-    if diff == 0:
-        computer = computer + 1
-        player = player + 1
-        print("***Player and computer tie!***")
-        print()
-    elif (diff == 1) or (diff == 2):
-        computer = computer + 1
-        print("***Computer wins!***")
-        print()
-    else:
-        player = player + 1
-        print("***Player wins!***")
-        print()
 
-print("*********Score Board*********")
-print("No of Games Played:",n)
-print("Player Score:",player)
-print("Computer Score:",computer)
-
-if i == n-1:
-    if computer > player:
-        print("Computer wins the game over",n,"turns")
-    elif player > computer:
-        print("Player wins the game over",n,"turns")
-    else:
-        print("Game is tie over",n,"turns")
-
-
-
-
+play_game(num_turns)
